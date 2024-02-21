32c13c7de82b90c5ede268a741896e613ad7c3de
Abid <abid5533@gmail.com>
2024-02-21 23:53:22 +0600

    auth.guard added for unauthorized access

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
@@ -14,19 +15,19 @@ export class NavComponent implements OnInit {
   model:any ={};
   currentUser$: Observable<User|null> = of(null);
 
-  constructor(private accountService: AccountService, private router:Router){}
+  constructor(private accountService: AccountService, private router:Router,private toastr:ToastrService){}
 
   ngOnInit(): void {
     this.currentUser$ = this.accountService.currentUser$;
   }
 
-  
+
 
   login()
   {
     this.accountService.login(this.model).subscribe({
       next: ()=>this.router.navigateByUrl('/members'),
-      error: error=>console.log(error)
+      error: error=>this.toastr.error(error.error)
     })
 
   }
