f30ed41e6071a0d46afa38eaabd14f2d58d92218
Abid <abid.bd@asa-international.com>
2024-03-20 15:27:14 +0600

    user details added

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -2,14 +2,20 @@ import { MembersService } from 'src/app/_services/members.service';
 import { Member } from './../../_models/member';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-member-detail',
+  standalone:true,
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
+  imports:[CommonModule,TabsModule,GalleryModule]
 })
 export class MemberDetailComponent implements OnInit {
   member: Member | undefined;
+  images:GalleryItem[]=[];
 
 
   constructor(private membersService:MembersService, private route:ActivatedRoute){}
@@ -23,8 +29,20 @@ export class MemberDetailComponent implements OnInit {
     const username = this.route.snapshot.paramMap.get('username');
     if (!username) return;
     this.membersService.getMember(username).subscribe({
-      next:member=>this.member = member
+      next:member=>{
+        this.member = member,
+        this.getImages()
+      }
     })
+  }
+
+  getImages(){
+    if (!this.member) return;
+
+    for(const photo of this.member.photos){
+      this.images.push(new ImageItem({src:photo.url, thumb:photo.url}))
+    }
+
   }
 
 }
